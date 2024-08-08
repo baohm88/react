eb8a6ad9b3ff8bc88e7cb0203d7b772efadc3914
Bao Ha <baohm88@gmail.com>
2024-08-08 14:42:38 +0700

    added activePlayer and handleSelectSquare to App

--- s04/src/components/Player.jsx
+++ s04/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
   const [enterePlayerName, setEneteredPlayerName] = useState(name);
 
@@ -26,7 +26,7 @@ export default function Player({ name, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
