1bb5aeddd055ad752cd514a2063e26031aa30a7f
Bao Ha <baohm88@gmail.com>
2024-08-09 19:21:38 +0700

    added components

--- s05/src/App.jsx
+++ s05/src/App.jsx
@@ -1,10 +1,17 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <li>Player 1</li>
+          <li>Player 2</li>
+        </ol>
+        <p>game over</p>
+        <p>game board</p>
+      </div>
+      <p>log</p>
+    </main>
+  );
 }
 
-export default App
+export default App;
