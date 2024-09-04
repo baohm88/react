3ec9ad0c72705c4fdd5b2a2f5c7708a80a2977f6
Bao Ha <baohm88@gmail.com>
2024-09-04 09:14:23 +0700

    added HomePage & ProductsPage

--- s21/src/App.js
+++ s21/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ s21/src/pages/ProductsPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductsPage() {
+  return <h2>My Products</h2>;
+}
