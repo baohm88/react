9039a08d3b7e360b38279da966cd2ae3dd467877
Bao Ha <baohm88@gmail.com>
2024-09-06 10:36:26 +0700

    added logout

--- s22/frontend/src/pages/Authentication.js
+++ s22/frontend/src/pages/Authentication.js
@@ -7,7 +7,7 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-export async function action({ request, params }) {
+export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode") || "login";
 
@@ -15,7 +15,7 @@ export async function action({ request, params }) {
     throw json({ message: "Unsupported mode." }, { status: 422 });
   }
 
-  const data = request.formData();
+  const data = await request.formData();
   const authData = {
     email: data.get("email"),
     password: data.get("password"),
@@ -35,7 +35,13 @@ export async function action({ request, params }) {
     throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
 
-  // soon: manage that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
 
   return redirect("/");
 }
