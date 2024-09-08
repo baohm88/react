798dbcc910469a965dfc4a21e0475f6336f17d78
Bao Ha <baohm88@gmail.com>
2024-09-08 16:30:30 +0700

    added getUsers and output users

--- react-php/src/components/ListUser.jsx
+++ react-php/src/components/ListUser.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function ListUser() {
-  return <div>ListUser</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  function getUsers() {
+    axios.get("http://localhost:80/api/users/").then(function (response) {
+      console.log(response.data);
+      setUsers(response.data);
+    });
+  }
+
+  return (
+    <div>
+      <h1>List Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.mobile}</td>
+              <td>
+                <Link to={`user/${user.id}/edit`}>Edit</Link> |{" "}
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
