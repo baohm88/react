29b0d864f37a3f84824a58622e686c1417fc133d
Bao Ha <baohm88@gmail.com>
2024-10-02 16:17:15 +0700

    added initdb.js and meals.db

--- s25/app/meals/page.js
+++ s25/app/meals/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealGrids from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
     return (
@@ -17,7 +18,9 @@ export default function MealsPage() {
                     <Link href="/meals/share">Share your favorite recipe</Link>
                 </p>
             </header>
-            <main className={classes.main}></main>
+            <main className={classes.main}>
+                <MealGrids meals={[]} />
+            </main>
         </>
     );
 }
